7349f1925b5d69f90c327647259aad39279f6794
Jost Triller <tsoj.tsoj@gmx.de>
2025-04-28 00:26:09 +0200

    LLM-generated improvement: request-10 Bench: 1916451

--- src/search.cpp
+++ src/search.cpp
@@ -1107,9 +1107,11 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth += history / 3388;
 
+                // Add a small margin against pruning if static eval is already > alpha
+                int futilityMarginBonus = std::max(0, int(ss->staticEval - alpha)) / 4;
+
                 Value futilityValue =
-                  ss->staticEval + (bestMove ? 46 : 138) + 117 * lmrDepth
-                  + 102 * (bestValue < ss->staticEval - 127 && ss->staticEval > alpha - 50);
+                  ss->staticEval + (bestMove ? 46 : 138) + 117 * lmrDepth + futilityMarginBonus;
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
