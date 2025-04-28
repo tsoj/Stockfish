8a8184cca227948a559af42f8e034d7fb9348d16
Jost Triller <tsoj.tsoj@gmx.de>
2025-04-29 01:55:52 +0200

    LLM-generated improvement: request-7 Bench: 2174695

--- src/search.cpp
+++ src/search.cpp
@@ -1124,8 +1124,8 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth = std::max(lmrDepth, 0);
 
-                // Prune moves with negative SEE
-                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth))
+                // Prune moves with negative SEE. Be slightly less aggressive in PV nodes.
+                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth + PvNode * 40))
                     continue;
             }
         }
