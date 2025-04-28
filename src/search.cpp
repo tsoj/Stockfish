7814138e8e8cc7a4cd2a46797f972bffcde52fd9
Jost Triller <tsoj.tsoj@gmx.de>
2025-04-28 13:33:57 +0200

    LLM-generated improvement: request-3 Bench: 13408812

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop:  // When in check, search starts here
             if (!rootNode && move == ttData.move && !excludedMove
                 && depth >= 6 - (thisThread->completedDepth > 27) + ss->ttPv
                 && is_valid(ttData.value) && !is_decisive(ttData.value)
-                && (ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 3)
+                && (ttData.bound & (BOUND_LOWER | BOUND_EXACT)) && ttData.depth >= depth - 3)
             {
                 Value singularBeta  = ttData.value - (58 + 76 * (ss->ttPv && !PvNode)) * depth / 57;
                 Depth singularDepth = newDepth / 2;
