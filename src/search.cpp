500932ad0faa3b7cdd6133881795fd3cfae8252f
Jost Triller <tsoj.tsoj@gmx.de>
2025-04-29 13:48:06 +0200

    LLM-generated improvement: request-3 Bench: 2007345

--- src/search.cpp
+++ src/search.cpp
@@ -1124,8 +1124,8 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth = std::max(lmrDepth, 0);
 
-                // Prune moves with negative SEE
-                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth))
+                // Prune moves with negative SEE. Be slightly more aggressive in cut nodes.
+                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth + 50 * cutNode))
                     continue;
             }
         }
