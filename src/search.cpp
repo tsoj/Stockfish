61a0cea8f65bda6b18c9dc891d7333b6b3c0ad52
Jost Triller <tsoj.tsoj@gmx.de>
2025-04-30 01:19:35 +0200

    LLM-generated improvement: request-11 Bench: 2144883

--- src/search.cpp
+++ src/search.cpp
@@ -1286,8 +1286,10 @@ moves_loop:  // When in check, search starts here
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
+                // Maintain the original node type (cutNode or not) during LMR re-search.
+                // If the original node was a cutNode, the re-search will also prune aggressively.
                 if (newDepth > d)
-                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, cutNode);
 
                 // Post LMR continuation history updates
                 update_continuation_histories(ss, movedPiece, move.to_sq(), 1508);
