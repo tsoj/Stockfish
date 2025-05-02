156b0f1184a11207c918a38ebf059ab044cedbdb
Jost Triller <tsoj.tsoj@gmx.de>
2025-05-03 01:59:28 +0200

    LLM-generated improvement: request-3 Bench: 1990601

--- src/search.cpp
+++ src/search.cpp
@@ -1717,8 +1717,8 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                      <= 6218)
                 continue;
 
-            // Do not search moves with bad enough SEE values
-            if (!pos.see_ge(move, -74))
+            // Do not search captures with bad enough SEE values
+            if (capture && !pos.see_ge(move, -74))
                 continue;
         }
 
