80936bd7a10fe01542d7144690b75f152d155660
Jost Triller <tsoj.tsoj@gmx.de>
2025-05-02 16:26:58 +0200

    doDeeperSearch after LMR only if additionally the value exceeds alpha by a margin. Bench: 1765080

--- src/search.cpp
+++ src/search.cpp
@@ -1281,7 +1281,8 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);
+                const bool doDeeperSearch =
+                  value > (bestValue + 42 + 2 * newDepth) && value > (alpha + 3 * newDepth);
                 const bool doShallowerSearch = value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
