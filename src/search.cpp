1a044f4778e3401cd34cd02792d64012ccbda5c4
Jost Triller <tsoj.tsoj@gmx.de>
2025-05-02 23:02:31 +0200

    Add prior quiet countermove history bonus if in current fail low node the static eval aligns with the bad bestValue Bench: 2023499

--- src/search.cpp
+++ src/search.cpp
@@ -1477,7 +1477,7 @@ moves_loop:  // When in check, search starts here
         bonusScale += 174 * ((ss - 1)->moveCount > 8);
         bonusScale += 86 * (ss - 1)->isTTMove;
         bonusScale += 90 * (ss->cutoffCnt <= 3);
-        bonusScale += 144 * (!ss->inCheck && bestValue <= ss->staticEval - 104);
+        bonusScale += 110 * (!ss->inCheck && bestValue > ss->staticEval - 160);
         bonusScale += 128 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 82);
 
         bonusScale = std::max(bonusScale, 0);
