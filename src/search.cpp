e380993769135093a2304bf542130221495889ec
Jost Triller <tsoj.tsoj@gmx.de>
2025-05-07 20:15:42 +0200

    Decrease less in pv node at priorReduction reduction Bench: 1857323

--- src/search.cpp
+++ src/search.cpp
@@ -853,7 +853,8 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 175)
+    if (priorReduction >= 1 && depth >= 2
+        && ss->staticEval + (ss - 1)->staticEval > (175 + 40 * PvNode))
         depth--;
 
     // Step 7. Razoring
