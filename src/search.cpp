f3cf91a1e85c8311db33cecf32efbfbce64ba32c
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-15 20:35:39 +0200

    LLM-generated improvement: qsearch_multiply_continuation_history_pruning Bench: 2192512

--- src/search.cpp
+++ src/search.cpp
@@ -1667,6 +1667,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             // Continuation history based pruning
             if (!capture
                 && (*contHist[0])[pos.moved_piece(move)][move.to_sq()]
+                       + (*contHist[1])[pos.moved_piece(move)][move.to_sq()]
                        + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)]
                                                 [move.to_sq()]
                      <= 6218)
