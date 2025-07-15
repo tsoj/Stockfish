24db8b26b999835cfe00e476d4d46df3c057c6a7
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-15 13:27:09 +0200

    LLM-generated improvement: see_pruning_less_before_bestmove Bench: 1914218

--- src/search.cpp
+++ src/search.cpp
@@ -1102,8 +1102,8 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth = std::max(lmrDepth, 0);
 
-                // Prune moves with negative SEE
-                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth))
+                // Prune moves with negative SEE. Be more careful before a best move is found.
+                if (!pos.see_ge(move, -(27 + 9 * !bestMove) * lmrDepth * lmrDepth))
                     continue;
             }
         }
