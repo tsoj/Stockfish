cabdde95e0b49eca088e5012c9b68abab0d89bf8
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-15 18:49:54 +0200

    LLM-generated improvement: see_cutnode_adjust Bench: 2216557

--- src/search.cpp
+++ src/search.cpp
@@ -1103,7 +1103,7 @@ moves_loop:  // When in check, search starts here
                 lmrDepth = std::max(lmrDepth, 0);
 
                 // Prune moves with negative SEE
-                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth))
+                if (!pos.see_ge(move, -27 * lmrDepth * lmrDepth + 5 * cutNode))
                     continue;
             }
         }
