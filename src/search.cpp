4de8ec4b45dd5dc9acea8f86c7daa4668d169927
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-16 22:42:44 +0200

    LLM-generated improvement: request-4_singular_improving_margin_scaler Bench: 2454635

--- src/search.cpp
+++ src/search.cpp
@@ -1136,9 +1136,10 @@ moves_loop:  // When in check, search starts here
                 int corrValAdj   = std::abs(correctionValue) / 248400;
                 int doubleMargin = -4 + 244 * PvNode - 206 * !ttCapture - corrValAdj
                                  - 997 * ttMoveHistory / 131072
-                                 - (ss->ply > thisThread->rootDepth) * 47;
+                                 - (ss->ply > thisThread->rootDepth) * 47 - improving * (depth / 4);
                 int tripleMargin = 84 + 269 * PvNode - 253 * !ttCapture + 91 * ss->ttPv - corrValAdj
-                                 - (ss->ply * 2 > thisThread->rootDepth * 3) * 54;
+                                 - (ss->ply * 2 > thisThread->rootDepth * 3) * 54
+                                 - improving * (depth / 3);
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
