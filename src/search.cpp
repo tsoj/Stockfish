1683d42a9e40daa3d230816dc3a647fe87f1e12b
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-16 19:33:20 +0200

    LLM-generated improvement: request-1_probcut-depth-scaling Bench: 2156122

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 400;
+    probCutBeta = beta + 400 * (8 + depth) / 16;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
