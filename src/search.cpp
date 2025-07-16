022f5605f37ccc4a1ea2f10a648ae2e42334bd60
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-16 19:54:39 +0200

    LLM-generated improvement: request-4_dynamic_lmr_continuation_bonus Bench: 2302076

--- src/search.cpp
+++ src/search.cpp
@@ -1260,7 +1260,10 @@ moves_loop:  // When in check, search starts here
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates
-                update_continuation_histories(ss, movedPiece, move.to_sq(), 1508);
+                int bonus = 1508;
+                if (value > alpha)
+                    bonus = bonus + 96 * std::min((value - alpha) / 128, 8);
+                update_continuation_histories(ss, movedPiece, move.to_sq(), bonus);
             }
             else if (value > alpha && value < bestValue + 9)
                 newDepth--;
