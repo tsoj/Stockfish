dae5e2b605aac4e44830c7e98736573a1c4290a7
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-17 10:01:44 +0200

    LLM-generated improvement: request-2_lmr_cutnode_ttcapture_malus Bench: 2303845

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 2864 + 966 * !ttData.move;
+            r += 2864 + 966 * !ttData.move - 966 * (ttData.move && ttCapture);
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
