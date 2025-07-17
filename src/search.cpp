f59aab2980d6125f3c547d401fdefa2d9326907e
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-17 21:06:30 +0200

    LLM-generated improvement: request-6_lmr_extra_reduction_high_cutoffcnt Bench: 2197050

--- src/search.cpp
+++ src/search.cpp
@@ -1207,7 +1207,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
-            r += 1036 + allNode * 848;
+            r += 1036 + allNode * 848 + 512 * ((ss + 1)->cutoffCnt > 5);
 
         r += (ss + 1)->quietMoveStreak * 50;
 
