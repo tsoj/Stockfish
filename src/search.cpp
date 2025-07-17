8dfee49a712fdf808e885744d75b93e4c4378321
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-17 18:27:21 +0200

    LLM-generated improvement: request-2_history_no_pv_penalty Bench: 2325051

--- src/search.cpp
+++ src/search.cpp
@@ -809,10 +809,14 @@ Value Search::Worker::search(
     if (((ss - 1)->currentMove).is_ok() && !(ss - 1)->inCheck && !priorCapture && !ttHit)
     {
         int bonus = std::clamp(-10 * int((ss - 1)->staticEval + ss->staticEval), -1858, 1492) + 661;
-        thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus * 1057 / 1024;
-        if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
-            thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
-              << bonus * 1266 / 1024;
+        if (!(ss->isPvNode && bonus < 0))
+        {
+            thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus * 1057 / 1024;
+            if (type_of(pos.piece_on(prevSq)) != PAWN
+                && ((ss - 1)->currentMove).type_of() != PROMOTION)
+                thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
+                  << bonus * 1266 / 1024;
+        }
     }
 
     // Set up the improving flag, which is true if current static evaluation is
