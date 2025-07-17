d58ec7676188e07ab84fbfe34ef4399458eb74b1
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-17 09:52:01 +0200

    LLM-generated improvement: request-4_scale_lmr_deeper_threshold_by_statscore Bench: 2101747

--- src/search.cpp
+++ src/search.cpp
@@ -1251,7 +1251,9 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);
+                const bool doDeeperSearch =
+                  value
+                  > (bestValue + 42 + 2 * newDepth - std::min(ss->statScore / 4096, 5 + improving));
                 const bool doShallowerSearch = value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
