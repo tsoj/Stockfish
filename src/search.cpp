fed3840df250d0d143a809ce58429e03d9839ddf
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-17 18:27:36 +0200

    LLM-generated improvement: request-5_statscore_pawn_hist Bench: 2230465

--- src/search.cpp
+++ src/search.cpp
@@ -1221,9 +1221,10 @@ moves_loop:  // When in check, search starts here
               + thisThread->captureHistory[movedPiece][move.to_sq()][type_of(pos.captured_piece())]
               - 5030;
         else
-            ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
-                          + (*contHist[0])[movedPiece][move.to_sq()]
-                          + (*contHist[1])[movedPiece][move.to_sq()] - 3206;
+            ss->statScore =
+              2 * thisThread->mainHistory[us][move.from_to()]
+              + (*contHist[0])[movedPiece][move.to_sq()] + (*contHist[1])[movedPiece][move.to_sq()]
+              + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()] - 3206;
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 826 / 8192;
