5d93f001029baa7bb0d01ff26b538e91053fb28e
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-20 04:04:31 +0200

    LLM-generated improvement: request-2_conditional_singular_depth_increment Bench: 1785976

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth++;
+                depth += (ss->ttPv || extension > 1);
             }
 
             // Multi-cut pruning
