c47a0341fdd88bf07fee12d2522b7bf36a45232b
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-21 04:23:06 +0200

    LLM-generated improvement: request-1_futility_add_improving_worsening Bench: 2196116

--- src/search.cpp
+++ src/search.cpp
@@ -1085,9 +1085,10 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth += history / 3388;
 
-                Value baseFutility = (bestMove ? 46 : 230);
-                Value futilityValue =
-                  ss->staticEval + baseFutility + 117 * lmrDepth + 102 * (ss->staticEval > alpha);
+                const Value baseFutility  = (bestMove ? 46 : 230);
+                const Value futilityValue = ss->staticEval + baseFutility + 117 * lmrDepth
+                                          + 102 * (ss->staticEval > alpha) + 95 * improving
+                                          + 53 * opponentWorsening;
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
