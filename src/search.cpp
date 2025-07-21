f07f31d177be7279afed3b45f02a272082c870cf
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-21 04:23:36 +0200

    LLM-generated improvement: request-7_neg_ext_by_move_type Bench: 1893762

--- src/search.cpp
+++ src/search.cpp
@@ -1164,12 +1164,12 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
-                extension = -3;
+                extension = -3 - !ttCapture;
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
             else if (cutNode)
-                extension = -2;
+                extension = -2 - !ttCapture;
         }
 
         // Step 16. Make the move
