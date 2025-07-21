48d6d612a69dc5abd132b12ed45814a209ca3979
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-21 14:25:26 +0200

    Using 3 ply conthist for continuation history pruning as well Bench: 1914435

--- src/search.cpp
+++ src/search.cpp
@@ -1078,7 +1078,9 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4229 * depth)
+                if (history < -4229 * depth
+                    && ((*contHist[3])[movedPiece][move.to_sq()] < -4229 * depth / 4 || !cutNode
+                        || (ss + 1)->cutoffCnt <= 3))
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
