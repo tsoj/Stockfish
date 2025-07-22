2f9dd489e49cfa81d7f861cd5aa0b1ca42186e22
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-23 00:11:50 +0200

    LLM-generated improvement: request-9_dynamic_tt_razor_margin Bench: 2216743

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 400;
+    probCutBeta = beta + 400 + opponentWorsening * 40 - (ss - 1)->statScore / 1024;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
