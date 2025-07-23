9c3e03a5ac28e8c5cfc55965d7e8f6906e94d9db
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-23 23:36:42 +0200

    LLM-generated improvement: request-2_augment_improving_with_tt_improving Bench: 1946665

--- src/search.cpp
+++ src/search.cpp
@@ -819,7 +819,7 @@ Value Search::Worker::search(
     // bigger than the previous static evaluation at our turn (if we were in
     // check at our previous move we go back until we weren't in check) and is
     // false otherwise. The improving flag is used in various pruning heuristics.
-    improving = ss->staticEval > (ss - 2)->staticEval;
+    improving = ss->staticEval > (ss - 2)->staticEval || (ttHit && ttData.ttImproving);
 
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
