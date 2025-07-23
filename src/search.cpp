88bb1344f6aa42a5affb643833a6e41494d459dc
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-24 00:03:19 +0200

    LLM-generated improvement: request-3_lmr_tt_improving_adjustment Bench: 2012646

--- src/search.cpp
+++ src/search.cpp
@@ -1215,6 +1215,10 @@ moves_loop:  // When in check, search starts here
         if (move == ttData.move)
             r -= 2006;
 
+        // Decrease reduction if TT shows improving trend but static eval does not
+        if (ttData.ttImproving && !improving)
+            r -= 1024 - cutNode * 512;
+
         if (capture)
             ss->statScore =
               826 * int(PieceValue[pos.captured_piece()]) / 128
