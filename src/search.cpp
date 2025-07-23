57160bf0a68dc0d273b42088c44d8a9461d582e6
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-24 00:03:33 +0200

    LLM-generated improvement: request-7_singular-negative-extension-improving Bench: 2120637

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,7 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
-                extension = -3;
+                extension = -3 + ttData.ttImproving;
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
