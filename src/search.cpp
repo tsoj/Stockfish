1dae85307342e14901416468ae8869616172c530
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-23 23:37:09 +0200

    LLM-generated improvement: request-10_lmr-adjust-shallower-ttimproving Bench: 2125208

--- src/search.cpp
+++ src/search.cpp
@@ -1252,7 +1252,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doShallowerSearch = value < bestValue + 9 + ttData.ttImproving;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
