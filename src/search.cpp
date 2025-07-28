dbd6c62e734713471ebfb78b7a7b04e73599dceb
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-28 23:27:50 +0200

    LLM-generated improvement: request-8_lmr_depth_threshold_adaptation Bench: 2502331

--- src/search.cpp
+++ src/search.cpp
@@ -1215,7 +1215,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 826 / 8192;
 
         // Step 17. Late moves reduction / extension (LMR)
-        if (depth >= 2 && moveCount > 1)
+        if (depth >= 2 + PvNode + ss->inCheck && moveCount > 1)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
