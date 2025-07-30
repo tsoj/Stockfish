281c836ca736950afafc56169e6022aa4cd69be1
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-30 11:07:43 +0200

    Doing deeper LMR re-searches if value exceeds alpha by a lot, and shallower re-searches if value exceeds alpha only very little Bench: 2445091

--- src/search.cpp
+++ src/search.cpp
@@ -1235,7 +1235,14 @@ moves_loop:  // When in check, search starts here
                 const bool doDeeperSearch    = value > (bestValue + 43 + 2 * newDepth);
                 const bool doShallowerSearch = value < bestValue + 9;
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                // Adaptive re-search depth based on how much LMR exceeded alpha
+                int reSearchDepthAdjustment = 0;
+                if (value > alpha + 100 + 20 * depth)
+                    reSearchDepthAdjustment = 1;
+                else if (value < alpha + 25)
+                    reSearchDepthAdjustment = -1;
+
+                newDepth += doDeeperSearch - doShallowerSearch + reSearchDepthAdjustment;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
