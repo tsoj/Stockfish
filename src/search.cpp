a701e46db0aa044fd79c045b82d98b87eca44e61
Jost Triller <tsoj.tsoj@gmx.de>
2025-07-30 04:09:59 +0200

    LLM-generated improvement: request-18_extend_continuation_history_to_3_plies Bench: 3325812

--- src/search.cpp
+++ src/search.cpp
@@ -1206,7 +1206,8 @@ moves_loop:  // When in check, search starts here
         else
             ss->statScore = 2 * mainHistory[us][move.from_to()]
                           + (*contHist[0])[movedPiece][move.to_sq()]
-                          + (*contHist[1])[movedPiece][move.to_sq()];
+                          + (*contHist[1])[movedPiece][move.to_sq()]
+                          + (*contHist[2])[movedPiece][move.to_sq()] / 2;
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 789 / 8192;
