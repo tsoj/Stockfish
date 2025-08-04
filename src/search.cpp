b4b0b6cdbed67bd0835a7b23fa2af074f1eba41c
Jost Triller <tsoj.tsoj@gmx.de>
2025-08-04 22:11:09 +0200

    LLM-generated improvement: request-11_lmr_extended_continuation_history_scaling Bench: 3013982

--- src/search.cpp
+++ src/search.cpp
@@ -1207,7 +1207,9 @@ moves_loop:  // When in check, search starts here
         else
             ss->statScore = 2 * mainHistory[us][move.from_to()]
                           + (*contHist[0])[movedPiece][move.to_sq()]
-                          + (*contHist[1])[movedPiece][move.to_sq()];
+                          + (*contHist[1])[movedPiece][move.to_sq()]
+                          + (*contHist[2])[movedPiece][move.to_sq()] / 2
+                          + (*contHist[3])[movedPiece][move.to_sq()] / 4;
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 789 / 8192;
