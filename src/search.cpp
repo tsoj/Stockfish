1322419545352c9b6ab86421df3fe490ec1475f4
Jost Triller <tsoj.tsoj@gmx.de>
2025-08-06 03:02:31 +0200

    LLM-generated improvement: request-18_pv_node_negative_extension_scaling Bench: 3055724

--- src/search.cpp
+++ src/search.cpp
@@ -1155,12 +1155,12 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
-                extension = -3;
+                extension = -3 + PvNode;
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
             else if (cutNode)
-                extension = -2;
+                extension = -2 + PvNode;
         }
 
         // Step 16. Make the move
