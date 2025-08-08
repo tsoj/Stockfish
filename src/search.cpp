485a45edc258c96563800570f7c50b8edbc56866
Jost Triller <tsoj.tsoj@gmx.de>
2025-08-09 01:32:00 +0200

    LLM-generated improvement: request-13_adaptive_singular_beta_margin_scaling Bench: 2858126

--- src/search.cpp
+++ src/search.cpp
@@ -1116,7 +1116,8 @@ moves_loop:  // When in check, search starts here
             && !is_decisive(ttData.value) && (ttData.bound & BOUND_LOWER)
             && ttData.depth >= depth - 3)
         {
-            Value singularBeta  = ttData.value - (56 + 79 * (ss->ttPv && !PvNode)) * depth / 58;
+            Value singularBeta =
+              ttData.value - (56 + 79 * (ss->ttPv && !PvNode)) * depth / (58 + depth / 8);
             Depth singularDepth = newDepth / 2;
 
             ss->excludedMove = move;
