cf2ced5da245a36f8de9eda8569deb488adcd165
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 20:03:11 +0100

    LLM-generated improvement: request-1_disable_rare_aggressive_cutoff_ltc Bench: 2597659

--- src/search.cpp
+++ src/search.cpp
@@ -963,8 +963,9 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
     probCutBeta = beta + 418;
-    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
-        && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
+    if (false && (ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4
+        && ttData.value >= probCutBeta && !is_decisive(beta) && is_valid(ttData.value)
+        && !is_decisive(ttData.value))
         return probCutBeta;
 
     const PieceToHistory* contHist[] = {
