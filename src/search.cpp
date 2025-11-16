d898b3b91e8bf84c97f3705b8fbe8ecdfafaebba
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 16:54:28 +0100

    LLM-generated improvement: request-1_extra-non-capture-reduction-lmr-skip Bench: 2662393

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop:  // When in check, search starts here
         {
             // Increase reduction if ttMove is not present
             if (!ttData.move)
-                r += 1118;
+                r += 1118 + 1024 * !capture;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
