aac15354872dec3908f7c59b1b7c9a2426e8048e
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 14:56:26 +0100

    LLM-generated improvement: request-1_singext_local_extension Bench: 2612983

--- src/search.cpp
+++ src/search.cpp
@@ -1127,8 +1127,6 @@ moves_loop:  // When in check, search starts here
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
-
-                depth++;
             }
 
             // Multi-cut pruning
