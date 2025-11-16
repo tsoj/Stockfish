811b4e91443ebaafd85f7e4d597f8273135e8644
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 22:37:47 +0100

    LLM-generated improvement: request-1_qsearch_ply_scaled_see_pruning Bench: 2352644

--- src/search.cpp
+++ src/search.cpp
@@ -1638,7 +1638,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 continue;
 
             // Do not search moves with bad enough SEE values
-            if (!pos.see_ge(move, -78))
+            if (!pos.see_ge(move, -145 + 5 * ss->ply))
                 continue;
         }
 
