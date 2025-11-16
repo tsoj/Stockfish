2d34581cedbfbe44564f47861a7ee3fdd138588e
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 20:42:31 +0100

    LLM-generated improvement: request-1_tt_cutoff_deeper_on_distant_fail_low Bench: 2910503

--- src/search.cpp
+++ src/search.cpp
@@ -690,7 +690,10 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove
+        && ttData.depth
+             > depth
+                 - ((ttData.value <= beta) + (ttData.value < alpha && alpha - ttData.value > 256))
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 5))
