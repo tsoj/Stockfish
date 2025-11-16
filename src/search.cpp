c25d497ff7f9a61ad77684a678f4d19eb2484731
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 19:57:44 +0100

    LLM-generated improvement: request-1_nuanced_lmr_depth_adjustment_scaler Bench: 2148139

--- src/search.cpp
+++ src/search.cpp
@@ -1225,16 +1225,20 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch = d < newDepth && value > (bestValue + 43 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 9;
+                int depthAdjustment = 0;
+                if (d < newDepth && value > bestValue + 43 + 2 * newDepth)
+                    depthAdjustment = std::min(2, (value - (bestValue + 43 + 2 * newDepth)) / 200);
+                else if (value < bestValue + 9)
+                    depthAdjustment = -1;
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += depthAdjustment;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates
-                update_continuation_histories(ss, movedPiece, move.to_sq(), 1365);
+                update_continuation_histories(ss, movedPiece, move.to_sq(),
+                                              1365 + 123 * depthAdjustment);
             }
         }
 
