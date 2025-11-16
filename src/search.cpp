cc81b5c5220e93967784a34922a71936135605a1
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 21:02:17 +0100

    LLM-generated improvement: request-1_singex_adaptive_verification_depth Bench: 2805219

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,7 @@ moves_loop:  // When in check, search starts here
             && ttData.depth >= depth - 3)
         {
             Value singularBeta  = ttData.value - (56 + 81 * (ss->ttPv && !PvNode)) * depth / 60;
-            Depth singularDepth = newDepth / 2;
+            Depth singularDepth = newDepth / 2 + (ttData.depth >= depth);
 
             ss->excludedMove = move;
             value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
