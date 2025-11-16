6c9297b095acf774b754fa34c88bcca8745d76ce
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-16 20:21:02 +0100

    LLM-generated improvement: request-1_pv_node_lmr_tactical_precision_scaler Bench: 2430589

--- src/search.cpp
+++ src/search.cpp
@@ -1206,7 +1206,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 794 / 8192;
 
         // Step 17. Late moves reduction / extension (LMR)
-        if (depth >= 2 && moveCount > 1)
+        if (depth >= 2 && moveCount > 1 + (PvNode && depth <= 8))
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
@@ -1239,7 +1239,7 @@ moves_loop:  // When in check, search starts here
         }
 
         // Step 18. Full-depth search when LMR is skipped
-        else if (!PvNode || moveCount > 1)
+        else if (!PvNode || moveCount > 1 + (PvNode && depth <= 8))
         {
             // Increase reduction if ttMove is not present
             if (!ttData.move)
