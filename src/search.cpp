c47fd6503d6f31c6ecf312bd358e81c9999eaace
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-18 19:37:31 +0100

    LLM-generated improvement: request-1_reduce_capture_penalty_in_tactical_positions Bench: 2995837

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
-            r += 1415;
+            r += 1415 - capture * 622;
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
