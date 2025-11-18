9dbc84942492445c9feaf7ee80833b612ae50382
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-18 19:20:10 +0100

    LLM-generated improvement: request-1_relax_tt_depth_cutnode Bench: 2421998

--- src/search.cpp
+++ src/search.cpp
@@ -690,7 +690,7 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta || cutNode)
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 5))
