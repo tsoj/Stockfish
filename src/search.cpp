1f19863463d51828d03f96ad0fb35a5c1fe7b83b
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-18 19:14:33 +0100

    LLM-generated improvement: request-1_lmr_research_cutnode_scale Bench: 2476849

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch = d < newDepth && value > (bestValue + 43 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doShallowerSearch = value < bestValue + 9 + 16 * cutNode;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
