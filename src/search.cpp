8f38ff79a3c00ad4ac2e780df0113f426c73fe67
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-18 18:39:47 +0100

    LLM-generated improvement: request-1_RemoveSECutNodeReduction Bench: 3048271

--- src/search.cpp
+++ src/search.cpp
@@ -1153,11 +1153,6 @@ moves_loop:  // When in check, search starts here
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
                 extension = -3;
-
-            // If we are on a cutNode but the ttMove is not assumed to fail high
-            // over current beta
-            else if (cutNode)
-                extension = -2;
         }
 
         // Step 16. Make the move
