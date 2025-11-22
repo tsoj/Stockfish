1e60cbee52cee5488ada741bd46be12b12c5a0e9
Jost Triller <tsoj.tsoj@gmx.de>
2025-11-22 18:27:08 +0100

    predicting if node will be cut node and reducing accordingly Bench: 2613281

--- src/search.cpp
+++ src/search.cpp
@@ -141,6 +141,25 @@ void update_all_stats(const Position& pos,
                       Move            TTMove,
                       int             moveCount);
 
+// Predicts probability of a fail-high using the small linear model trained
+float predict_fail_high(const bool cutNode,
+                        const bool ttUpperBound,
+                        const bool ttMoveIsNone,
+                        const bool improving,
+                        const bool inCheck,
+                        const bool ttLowerBound) {
+
+    const float logit = +0.383212f                        //
+                      + float(cutNode) * +0.745515f       //
+                      + float(ttUpperBound) * -0.740890f  //
+                      + float(improving) * +0.508265f     //
+                      + float(ttMoveIsNone) * -0.438759f  //
+                      + float(inCheck) * +0.377631f       //
+                      + float(ttLowerBound) * +0.050730f;
+
+    return 1.0 / (1.0 + std::exp(-logit));
+}
+
 }  // namespace
 
 Search::Worker::Worker(SharedState&                    sharedState,
@@ -978,6 +997,9 @@ moves_loop:  // When in check, search starts here
       (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
       (ss - 4)->continuationHistory, (ss - 5)->continuationHistory, (ss - 6)->continuationHistory};
 
+    const float predictedFailHigh =
+      predict_fail_high(cutNode, ss->ttHit && (ttData.bound & BOUND_UPPER), !ttData.move, improving,
+                        ss->inCheck, ss->ttHit && (ttData.bound & BOUND_LOWER));
 
     MovePicker mp(pos, ttData.move, depth, &mainHistory, &lowPlyHistory, &captureHistory, contHist,
                   &pawnHistory, ss->ply);
@@ -1210,6 +1232,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        if (predictedFailHigh > 0.6)
+            r -= 500;  // less reduction if we expect this node to be a cut node
+        else
+            r += 500;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
