6206f1bd489af4e187f8b2422ef9edf140bd0bce
Jost Triller <tsoj.tsoj@gmx.de>
2026-01-26 23:31:49 +0100

    Reduce less for cutnodes in LMR when the parent node didn't reduce Bench: 2609738

--- src/search.cpp
+++ src/search.cpp
@@ -1198,7 +1198,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 3372 + 997 * !ttData.move;
+            r += 3772 + 997 * !ttData.move - (priorReduction > 0) * 1000;
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
